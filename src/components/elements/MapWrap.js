9a716d889042dad5d885b61622a21ff88bcf65dc
Seongil-Shin <callmeshin75@gmail.com>
2021-09-22 18:09:32 +0900

    fix : api key

--- src/components/elements/MapWrap.js
+++ src/components/elements/MapWrap.js
@@ -28,9 +28,7 @@ const MapWrap = withScriptjs(
 export default function MapWrapWithScript({ coords }) {
    return (
       <MapWrap
-         googleMapURL={`https://maps.googleapis.com/maps/api/js?\
-     key=${process.env.REACT_APP_GOOGLEMAP_KEY}&\
-     v=3.exp&libraries=geometry,drawing,places`}
+         googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLEMAP_KEY}&v=3.exp&libraries=geometry,drawing,places`}
          loadingElement={<div style={{ height: `100%` }} />}
          containerElement={<div style={{ height: `400px` }} />}
          mapElement={<div style={{ height: `100%` }} />}
